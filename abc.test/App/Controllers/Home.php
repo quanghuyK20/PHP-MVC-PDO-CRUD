598a80bc244916a7147c60fee4261103877fc168
QuangHuy <lqhuy.vku@gmail.com>
2022-06-17 16:41:45 +0700

    first commit

---
+++ abc.test/App/Controllers/Home.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+class Home extends \Core\Controller
+{
+    protected function before()
+    {
+
+    }
+
+    protected function after()
+    {
+        
+    }
+
+    public function indexAction()
+    {
+
+        View::renderTemplate('Home/index.html', [
+            'name'    => 'Huy',
+            'colours' => ['red', 'Pink', 'blue']
+        ]);
+    }
+}
---
+++ abc.test/App/Controllers/Posts.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use App\Models\Post;
+
+class Posts extends \Core\Controller
+{
+
+    public function indexAction()
+    {
+        //echo 'Hello from the index action in the Posts controller!';
+        $posts = Post::getAll();
+        View::renderTemplate('Posts/index.html', [
+            'posts' => $posts
+        ]);
+    }
+
+    public function addNewAction()
+    {
+        echo 'Hello from the addNew action in the Posts controller!';
+    }
+
+    public function editAction()
+    {
+        echo 'Hello from the edit action in the Posts controller!';
+        echo '<p>Route parameters: <pre>' .
+             htmlspecialchars(print_r($this->route_params, true)) . '</pre></p>';
+    }
+}
